b3217d9753e67bd3ef3058deefb86244d154c9e8
GlazedPineapple <GlazedPineapple@users.noreply.github.com>
2020-02-16 18:00:21 +0000

    deploy: 166d1a08ec2f509ccc9f10a05dcdcf19f084728d

---
+++ precache-manifest.d61ef3ca56fc15a8d58ebb364a9ce80b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b35aa13e825fc537896b86fca6646d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ef6d686c4b1fd9c7165",
+    "url": "/static/js/2.8bb7596f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.8bb7596f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cb0ac3b35b86609d9270",
+    "url": "/static/js/main.0f4470c6.chunk.js"
+  },
+  {
+    "revision": "fe2756087d0e50caf018",
+    "url": "/static/js/runtime-main.00d0f866.js"
+  }
+]);
