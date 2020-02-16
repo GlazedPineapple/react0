2788c1fb97868347b2531f81cd0eaed8498a1643
Elliot <eokovacs1@gmail.com>
2020-02-15 20:06:42 -0500

    Made the weather stuff look better
    Co-authored-by: Zachary Kohnen <dusterthefirst@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import useWeather, { isError } from "./useWeather";
+import { kelvinToFahrenheit } from "./util";
 
 /** The main app */
 export default function App() {
@@ -24,6 +25,14 @@ export default function App() {
         // Success screen
         return (
             <>
+                <div>
+                    <div>Description: {apiresponse.weather[0].description}</div>
+                    <div>Name: {apiresponse.weather[0].main}</div>
+                    <div>Temp: {kelvinToFahrenheit(apiresponse.main.temp)}&deg;F</div>
+                    <div>Feels Like: {kelvinToFahrenheit(apiresponse.main.feels_like)}&deg;F</div>
+                    <div>Location: {apiresponse.name}</div>
+                    <img src={`http://openweathermap.org/img/w/${apiresponse.weather[0].icon}.png`}/>
+                </div>
                 <pre>{JSON.stringify(apiresponse, undefined, 4)}</pre>
             </>
         );
